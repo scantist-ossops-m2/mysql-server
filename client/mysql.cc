e90dc2270e9799c528177d21947ae803a4bbb178
admin-portal <admin-portal@scantist.com>
2024-06-12 20:38:55 +0800

    commit patch 5292920

--- client/mysql.cc
+++ client/mysql.cc
@@ -2286,6 +2286,8 @@ static bool add_line(String &buffer, char *line, ulong line_length,
     }
 #endif
     if (!*ml_comment && inchar == '\\' && *in_string != '`' &&
+        !(*in_string == '"' &&
+          (mysql.server_status & SERVER_STATUS_ANSI_QUOTES)) &&
         !(*in_string &&
           (mysql.server_status & SERVER_STATUS_NO_BACKSLASH_ESCAPES)))
     {
